8c5b14a6a5bf2dcffb07fed1d9f33ae73dfc64c7
IgorSevero <igor_s_goncalves@outlook.com>
2024-09-09 15:06:40 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,41 @@
+let dados = [
+    {
+        titulo : "Final Fantasy I", 
+        descricao : "A trama gira em torno de quatro guerreiros da luz, cada um carregando uma das quatro orbes elementais que foram corrompidas por forças das trevas. Em um mundo assolado pelo caos, os heróis embarcam em uma jornada épica para restaurar a luz às orbes, derrotar os monstros que as corromperam e salvar o reino da escuridão. O jogo introduziu elementos clássicos dos RPGs, como batalhas por turnos, um sistema de classes e um mundo vasto a ser explorado.",
+        link : "https://www.youtube.com/watch?v=zB9TwiXw6JM",
+        tags: "guerreiros luz"
+    },
+    {
+        titulo: "Final Fantasy II",
+        descricao: "Final Fantasy II é um RPG clássico que narra a história de quatro jovens que se unem para salvar o mundo de um império tirano. A trama se desenrola em um mundo devastado pela guerra, onde os personagens desenvolvem suas habilidades através do uso constante de suas armas e magias. O jogo introduz um sistema de progressão único, onde a força dos personagens aumenta com a prática, e não com a obtenção de pontos de experiência. A jornada épica dos heróis os leva a enfrentar desafios épicos e a descobrir a verdade por trás da guerra.", 
+        link: "https://www.youtube.com/watch?v=wcLAYQu1ojo&t=1s",
+        tags: "nolv"
+    },
+    {
+        titulo: "Final Fantasy III",
+        descricao: "Final Fantasy III é mais uma jornada épica que envolve a busca por restaurar o equilíbrio do mundo. A história gira em torno de quatro jovens que descobrem um cristal mágico e são escolhidos para se tornarem os novos guerreiros da luz. Com o mundo assolado por trevas e magia perdida, os heróis embarcam em uma aventura para despertar os poderes dos cristais e enfrentar poderosos inimigos. O jogo introduz um sistema de classes profundo, permitindo que os personagens mudem de profissão e desenvolvam habilidades únicas. A jornada dos quatro guerreiros os leva a explorar um mundo vasto, desvendar mistérios antigos e enfrentar desafios que colocarão suas habilidades à prova.",
+        link: "https://www.youtube.com/watch?v=b1pcrQdqmFg&t=4s",
+        tags: "cristal magico"
+    },
+    {
+        titulo: "Final Fantasy IV",
+        descricao: "Uma história épica de amor, perda e vingança, ambientada em um mundo de fantasia medieval. Cecil Harvey, um cavaleiro negro, embarca em uma jornada de autodescoberta após questionar as ordens de seu comandante. Ao lado de um grupo diversificado de companheiros, ele busca a redenção e luta para salvar o mundo de um cataclismo iminente. O jogo é conhecido por seus personagens complexos e por explorar temas maduros.",
+        link: "https://www.youtube.com/watch?v=vfaoWmPMEFU&list=PLhmtAP80_KE0lsaRFCCZ_fpIWInw8cQoN",
+        tags: "cavaleiro negro, vingança, amor"
+    },
+  
+    {
+        titulo: "Final Fantasy V",
+        descricao: "Um dos jogos mais elogiados da série, Final Fantasy V apresenta um sistema de classes altamente personalizável, permitindo que os jogadores combinem diferentes habilidades e criar personagens únicos. A história segue um grupo de heróis que devem impedir que um ser maligno conhecido como Exdeath destrua o mundo. O jogo se destaca por sua trilha sonora memorável e por sua jogabilidade inovadora.",
+        link: "https://www.youtube.com/watch?v=sh9WPaQp64A",
+        tags: "sistema de classes, exdeath, trilha sonora"
+    },
+  
+    {
+        titulo: "Final Fantasy VI",
+        descricao: "Considerado por muitos fãs como o melhor jogo da série, Final Fantasy VI apresenta uma história sombria e complexa, com personagens marcantes e um mundo em ruínas. A história se passa em um mundo pós-apocalíptico, onde um grupo de rebeldes luta para restaurar a paz. O jogo é conhecido por seus temas maduros, personagens complexos e por sua batalha final épica.",
+        link: "https://www.youtube.com/watch?v=Ebvm_Jz5Vu4",
+        tags: "pós-apocalíptico, personagens marcantes, batalha épica"
+    } 
+];
+
